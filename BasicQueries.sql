397e14c9cf956790f3b1075d40524eb6f33e6ff8
Raushan Manjhi <Raushan.manjhi@mycom-osi.com>
2023-06-05 14:52:04 +0530

    main

--- BasicQueries.sql
+++ BasicQueries.sql
@@ -10,3 +10,7 @@ TO DISK = 'C\Users\rmanjhi\Documents\GitHub\Backup\Document.bak';
 create table Employee(Id int, FirstName varchar(200),MiddleName varchar(100));
 --The DROP TABLE statement is used to drop an existing table in a database.
 drop table Employee
+--The TRUNCATE TABLE statement is used to delete the data inside a table, but not the table itself.
+TRUNCATE table Employee
+--The ALTER TABLE statement is used to add, delete, or modify columns and it is also used to add and drop various constraints in an existing table.
+alter table Employee ADD Email varchar(255);
